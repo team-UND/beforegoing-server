ace4bf8b85a720d966b8dcc4b0b8f6ca65c6f306
Chori <yidodeveloper@gmail.com>
2025-06-14 09:19:39 +0900

    ✨ Add fuctions related to JWT
    related to: #11

---
+++ src/main/java/com/und/server/jwt/JwtProperties.java
@@ -0,0 +1,23 @@
+package com.und.server.jwt;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import io.jsonwebtoken.security.Keys;
+
+@ConfigurationProperties(prefix = "jwt")
+public record JwtProperties(
+	String header,
+	String type,
+	String issuer,
+	String secret,
+	Integer accessTokenExpireTime,
+	Integer refreshTokenExpireTime
+) {
+
+	public SecretKey secretKey() {
+		return Keys.hmacShaKeyFor(secret.getBytes());
+	}
+
+}
