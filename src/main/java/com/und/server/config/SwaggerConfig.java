8a45c4b7bb63abf90daec4d9a61c169df5532534
Chori <yidodeveloper@gmail.com>
2025-06-15 22:01:03 +0900

    🔧 Enhance Swagger config with JWT security scheme

--- src/main/java/com/und/server/config/SwaggerConfig.java
+++ src/main/java/com/und/server/config/SwaggerConfig.java
@@ -6,14 +6,27 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
 
 	@Bean
 	public OpenAPI openApi() {
-		return new OpenAPI().components(new Components())
-			.info(apiInfo());
+		String jwt = "JWT";
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+		Components components = new Components().addSecuritySchemes(jwt,
+			new SecurityScheme()
+				.name(jwt)
+				.type(SecurityScheme.Type.HTTP)
+				.scheme("bearer")
+				.bearerFormat("JWT")
+		);
+		return new OpenAPI()
+			.info(apiInfo())
+			.addSecurityItem(securityRequirement)
+			.components(components);
 	}
 
 	private Info apiInfo() {
