34564806433eb2d0587598a9c014f91a42c18f03
choridev <yidodeveloper@gmail.com>
2025-07-15 19:04:25 +0900

    🎨 Add message to Null Validation

--- src/main/java/com/und/server/dto/AuthRequest.java
+++ src/main/java/com/und/server/dto/AuthRequest.java
@@ -1,11 +1,10 @@
 package com.und.server.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.und.server.oauth.Provider;
 
 import jakarta.validation.constraints.NotNull;
 
 public record AuthRequest(
-	@NotNull @JsonProperty("provider") Provider provider,
-	@NotNull @JsonProperty("id_token") String idToken
+	@NotNull(message = "Provider must not be null") @JsonProperty("provider") String provider,
+	@NotNull(message = "ID Token must not be null") @JsonProperty("id_token") String idToken
 ) { }
--- src/main/java/com/und/server/dto/HandshakeRequest.java
+++ src/main/java/com/und/server/dto/HandshakeRequest.java
@@ -1,10 +1,9 @@
 package com.und.server.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.und.server.oauth.Provider;
 
 import jakarta.validation.constraints.NotNull;
 
 public record HandshakeRequest(
-	@NotNull @JsonProperty ("provider") Provider provider
+	@NotNull(message = "Provider must not be null") @JsonProperty ("provider") String provider
 ) { }
