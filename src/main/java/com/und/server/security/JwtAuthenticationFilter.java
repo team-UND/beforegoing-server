bf5e6594d218c9bd72421a2bdc1eeed7ce6e7137
choridev <yidodeveloper@gmail.com>
2025-07-26 15:11:53 +0900

    🧑‍💻 Allow reissue tokens with expired token header

--- src/main/java/com/und/server/security/JwtAuthenticationFilter.java
+++ src/main/java/com/und/server/security/JwtAuthenticationFilter.java
@@ -1,12 +1,16 @@
 package com.und.server.security;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.und.server.exception.ServerErrorResult;
 import com.und.server.exception.ServerException;
 import com.und.server.jwt.JwtProvider;
 
@@ -22,20 +26,30 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private final JwtProvider jwtProvider;
 	private final SecurityErrorResponseWriter errorResponseWriter;
+	private final AntPathMatcher pathMatcher = new AntPathMatcher();
+	private final List<String> permissivePaths = Arrays.asList("/v*/auth/tokens");
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 		throws ServletException, IOException {
 
-		String token = resolveToken(request);
+		final String token = resolveToken(request);
 
 		if (token != null) {
 			try {
-				Authentication authentication = jwtProvider.getAuthentication(token);
+				final Authentication authentication = jwtProvider.getAuthentication(token);
 				SecurityContextHolder.getContext().setAuthentication(authentication);
 			} catch (ServerException e) {
-				errorResponseWriter.sendErrorResponse(response, e.getErrorResult());
-				return;
+				final boolean isPermissivePath = permissivePaths.stream().anyMatch(
+					pattern -> pathMatcher.match(pattern, request.getServletPath()));
+
+				if (e.getErrorResult() == ServerErrorResult.EXPIRED_TOKEN && isPermissivePath) {
+					// For expired tokens on permissive paths, do not stop the chain
+				} else {
+					// For other errors, stop the chain and write the error response
+					errorResponseWriter.sendErrorResponse(response, e.getErrorResult());
+					return;
+				}
 			}
 		}
 
@@ -43,7 +57,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	}
 
 	private String resolveToken(HttpServletRequest request) {
-		String bearerToken = request.getHeader("Authorization");
+		final String bearerToken = request.getHeader("Authorization");
 		if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
 			return bearerToken.substring(7);
 		}
