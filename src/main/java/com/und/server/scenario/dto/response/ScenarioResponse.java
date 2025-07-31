80af4372c062f147b2244809ff684f85318d2d1f
sotogito <sotogitoarchive@gmail.com>
2025-07-31 23:36:45 +0900

    🎨 Add ScenarioResponse DTO conversion static method

--- src/main/java/com/und/server/scenario/dto/response/ScenarioResponse.java
+++ src/main/java/com/und/server/scenario/dto/response/ScenarioResponse.java
@@ -1,7 +1,10 @@
 package com.und.server.scenario.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.und.server.scenario.entity.Scenario;
 import lombok.*;
+
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,5 +19,21 @@ public class ScenarioResponse {
 	private String scenarioName;
 	private String memo;
 	private Integer order;
+
+
+	public static ScenarioResponse of(Scenario scenario) {
+		return ScenarioResponse.builder()
+			.scenarioId(scenario.getId())
+			.scenarioName(scenario.getScenarioName())
+			.memo(scenario.getMemo())
+			.order(scenario.getOrder())
+			.build();
+	}
+
+	public static List<ScenarioResponse> listOf(List<Scenario> scenarioList) {
+		return scenarioList.stream()
+			.map(ScenarioResponse::of)
+			.toList();
+	}
 
 }
