dbfa039e1dbf940cee92d78cf5c1eec746574a5b
sotogito <sotogitoarchive@gmail.com>
2025-07-31 23:41:40 +0900

    ✅ Test return scenario list by member

---
+++ src/test/java/com/und/server/scenario/controller/ScenarioControllerTest.java
@@ -0,0 +1,77 @@
+package com.und.server.scenario.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.und.server.exception.GlobalExceptionHandler;
+import com.und.server.scenario.dto.response.ScenarioResponse;
+import com.und.server.scenario.service.ScenarioService;
+import com.und.server.security.AuthMemberArgumentResolver;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class ScenarioControllerTest {
+
+	@InjectMocks
+	private ScenarioController scenarioController;
+
+	@Mock
+	private AuthMemberArgumentResolver authMemberArgumentResolver;
+
+	@Mock
+	private ScenarioService scenarioService;
+
+	private MockMvc mockMvc;
+	private final ObjectMapper objectMapper = new ObjectMapper();
+
+	@BeforeEach
+	void setUp() {
+		mockMvc = MockMvcBuilders.standaloneSetup(scenarioController)
+			.setCustomArgumentResolvers(authMemberArgumentResolver)
+			.setControllerAdvice(new GlobalExceptionHandler())
+			.build();
+	}
+
+	@Test
+	void Given_MemberId_WHen_GetScenario_Than_ReturnScenario() throws Exception {
+		// given
+		final String url = "/v1/scenarios";
+		final Long memberId = 1L;
+		final List<ScenarioResponse> response = List.of(
+			ScenarioResponse.builder().scenarioId(1L).scenarioName("시나리오1").memo("메모1").order(1).build(),
+			ScenarioResponse.builder().scenarioId(2L).scenarioName("시나리오2").memo("메모2").order(2).build()
+		);
+
+		doReturn(true).when(authMemberArgumentResolver).supportsParameter(any());
+		doReturn(memberId).when(authMemberArgumentResolver).resolveArgument(any(), any(), any(), any());
+		doReturn(response).when(scenarioService).findScenariosByMemberId(memberId);
+
+		// when
+		final ResultActions resultActions = mockMvc.perform(
+			MockMvcRequestBuilders.get(url)
+				.accept(MediaType.APPLICATION_JSON)
+		);
+
+		// then
+		resultActions.andExpect(status().isOk())
+			.andExpect(jsonPath("$.length()").value(2))
+			.andExpect(jsonPath("$[0].scenarioId").value(1))
+			.andExpect(jsonPath("$[0].scenarioName").value("시나리오1"));
+	}
+
+}
---
+++ src/test/java/com/und/server/scenario/service/ScenarioServiceTest.java
@@ -0,0 +1,83 @@
+package com.und.server.scenario.service;
+
+import com.und.server.entity.Member;
+import com.und.server.notification.entity.NotifType;
+import com.und.server.notification.entity.Notification;
+import com.und.server.scenario.dto.response.ScenarioResponse;
+import com.und.server.scenario.entity.Scenario;
+import com.und.server.scenario.repository.ScenarioRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
+class ScenarioServiceTest {
+
+	@InjectMocks
+	private ScenarioService scenarioService;
+
+	@Mock
+	private ScenarioRepository scenarioRepository;
+
+	@Test
+	void Given_memberId_When_FindScenarios_Then_ReturnScenarios() {
+		//given
+		final Long memberId = 1L;
+
+		final Member member = Member.builder()
+			.id(memberId)
+			.build();
+
+		final Notification notification1 = Notification.builder()
+			.id(1L)
+			.isActive(true)
+			.notifType(NotifType.TIME)
+			.build();
+		final Notification notification2 = Notification.builder()
+			.id(2L)
+			.isActive(true)
+			.notifType(NotifType.TIME)
+			.build();
+
+		final Scenario scenarioA = Scenario.builder()
+			.id(1L)
+			.member(member)
+			.scenarioName("시나리오A")
+			.memo("메모A")
+			.order(1)
+			.notification(notification1)
+			.build();
+		final Scenario scenarioB = Scenario.builder()
+			.id(1L)
+			.member(member)
+			.scenarioName("시나리오B")
+			.memo("메모B")
+			.order(2)
+			.notification(notification2)
+			.build();
+
+		final List<Scenario> scenarioList = List.of(scenarioA, scenarioB);
+
+		//when
+		Mockito
+			.when(scenarioRepository.findByMemberIdOrderByOrder(memberId))
+			.thenReturn(scenarioList);
+
+		List<ScenarioResponse> result = scenarioService.findScenariosByMemberId(memberId);
+
+		//then
+		assertNotNull(result);
+		assertThat(result.size()).isEqualTo(2);
+		assertThat(result.get(0).getScenarioName()).isEqualTo("시나리오A");
+		assertThat(result.get(1).getScenarioName()).isEqualTo("시나리오B");
+	}
+
+}
