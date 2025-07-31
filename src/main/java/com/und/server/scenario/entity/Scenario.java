dce10c8f9ebad37595a81a9970bd1562700db86c
sotogito <sotogitoarchive@gmail.com>
2025-07-31 16:51:11 +0900

    ✨ Create Scenario entity

---
+++ src/main/java/com/und/server/scenario/entity/Scenario.java
@@ -0,0 +1,43 @@
+package com.und.server.scenario.entity;
+
+import com.und.server.entity.Member;
+import com.und.server.notification.entity.Notification;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+
+@Table
+@Entity
+public class Scenario extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@Column(nullable = false, length = 10)
+	private String scenarioName;
+
+	@Column(length = 15)
+	private String memo;
+
+	@Column(nullable = false)
+	private Integer order;
+
+	@OneToOne
+	@JoinColumn(name = "notification_id")
+	private Notification notification;
+
+	@OneToMany(mappedBy = "scenario", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Mission> missionList;
+
+}
