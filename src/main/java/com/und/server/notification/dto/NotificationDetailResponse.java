e162beb690338696cb8c852de97e4cf59541044c
sotogito <sotogitoarchive@gmail.com>
2025-08-01 17:13:28 +0900

    🎨 Refactor NotificationResponse

--- src/main/java/com/und/server/notification/dto/NotificationDetailResponse.java
+++ src/main/java/com/und/server/notification/dto/NotificationDetailResponse.java
@@ -2,7 +2,6 @@ package com.und.server.notification.dto;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.und.server.notification.entity.NotifType;
 
 @JsonTypeInfo(
 	use = JsonTypeInfo.Id.NAME,
@@ -12,8 +11,5 @@ import com.und.server.notification.entity.NotifType;
 @JsonSubTypes({
 	@JsonSubTypes.Type(value = TimeNotifResponse.class, name = "TIME")
 })
-public interface NotificationResponse {
-
-	NotifType getNotificationType();
-
+public abstract class NotificationDetailResponse {
 }
--- src/main/java/com/und/server/notification/dto/TimeNotifResponse.java
+++ src/main/java/com/und/server/notification/dto/TimeNotifResponse.java
@@ -1,8 +1,10 @@
 package com.und.server.notification.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.und.server.notification.entity.NotifType;
+import com.und.server.notification.entity.TimeNotif;
 import lombok.*;
+
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,18 +13,35 @@ import lombok.*;
 @Builder
 @ToString
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class TimeNotifResponse implements NotificationResponse{
+public class TimeNotifResponse extends NotificationDetailResponse {
 
-	private Long notificationId;
+	private Long timeNotificationId;
 	private Boolean isEveryDay;
 	private Integer dayOfWeekOrdinal;
 	private Integer hour;
 	private Integer minute;
 
 
-	@Override
-	public NotifType getNotificationType() {
-		return NotifType.TIME;
+	public static NotificationDetailResponse of(TimeNotif timeNotif) {
+		Integer dayOfWeekOrdinal = timeNotif.getDayOfWeek() != null ? timeNotif.getDayOfWeek().ordinal() : null;
+		Integer hour = timeNotif.getHour();
+		Integer minute = timeNotif.getMinute();
+
+		boolean isEveryDay = dayOfWeekOrdinal == null && hour == null && minute == null;
+
+		return TimeNotifResponse.builder()
+			.timeNotificationId(timeNotif.getId())
+			.isEveryDay(isEveryDay)
+			.dayOfWeekOrdinal(dayOfWeekOrdinal)
+			.hour(hour)
+			.minute(minute)
+			.build();
+	}
+
+	public static List<NotificationDetailResponse> listOf(List<TimeNotif> timeNotifList) {
+		return timeNotifList.stream()
+			.map(TimeNotifResponse::of)
+			.toList();
 	}
 
 }
