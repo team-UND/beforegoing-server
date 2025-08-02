3ac223ded8f637c4503fc7cade73de5d33142907
sotogito <sotogitoarchive@gmail.com>
2025-08-02 16:32:57 +0900

    ✨ Add notification error

---
+++ src/main/java/com/und/server/notification/exception/NotificationErrorResult.java
@@ -0,0 +1,21 @@
+package com.und.server.notification.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.und.server.common.exception.ErrorResult;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum NotificationErrorResult implements ErrorResult {
+
+	UNSUPPORTED_NOTIFICATION(
+		HttpStatus.BAD_REQUEST, "Unsupported notification type");
+
+
+	private final HttpStatus httpStatus;
+	private final String message;
+
+}
