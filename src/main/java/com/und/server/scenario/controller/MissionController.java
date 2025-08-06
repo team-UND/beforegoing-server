a8f8916f4967e942789b661c2101211cc51604bd
sotogito <sotogitoarchive@gmail.com>
2025-08-06 23:07:10 +0900

    🐛 Add NotificationType when get mission list

--- src/main/java/com/und/server/scenario/controller/MissionController.java
+++ src/main/java/com/und/server/scenario/controller/MissionController.java
@@ -11,9 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.und.server.auth.filter.AuthMember;
-import com.und.server.scenario.dto.requeset.MissionAddRequest;
+import com.und.server.scenario.dto.requeset.TodayMissionRequest;
 import com.und.server.scenario.dto.response.MissionGroupResponse;
 import com.und.server.scenario.service.MissionService;
+import com.und.server.scenario.service.ScenarioService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -24,12 +25,15 @@ import lombok.RequiredArgsConstructor;
 @RestController
 public class MissionController {
 
+	private final ScenarioService scenarioService;
 	private final MissionService missionService;
 
 
 	@GetMapping("/scenarios/{scenarioId}/missions")
 	public ResponseEntity<MissionGroupResponse> getMissionsByScenarioId(
-		@AuthMember Long memberId, @PathVariable Long scenarioId) {
+		@AuthMember Long memberId,
+		@PathVariable Long scenarioId
+	) {
 		MissionGroupResponse missionList = missionService.findMissionsByScenarioId(memberId, scenarioId);
 
 		return ResponseEntity.ok().body(missionList);
@@ -40,9 +44,9 @@ public class MissionController {
 	public ResponseEntity<Void> addTodayMissionToScenario(
 		@AuthMember Long memberId,
 		@PathVariable Long scenarioId,
-		@RequestBody @Valid MissionAddRequest missionAddRequest
+		@RequestBody @Valid TodayMissionRequest missionAddRequest
 	) {
-		missionService.addMissionToScenario(memberId, scenarioId, missionAddRequest);
+		scenarioService.addTodayMissionToScenario(memberId, scenarioId, missionAddRequest);
 
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
