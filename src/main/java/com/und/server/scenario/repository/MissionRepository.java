3c32ee99eaa66fd675f8e85afa1eb1ec06af2b13
sotogito <sotogitoarchive@gmail.com>
2025-08-08 20:47:01 +0900

    🎨 Add memberId condition

--- src/main/java/com/und/server/scenario/repository/MissionRepository.java
+++ src/main/java/com/und/server/scenario/repository/MissionRepository.java
@@ -1,15 +1,31 @@
 package com.und.server.scenario.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.und.server.scenario.entity.Mission;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
 
-	@EntityGraph(attributePaths = {"scenario"})
-	List<Mission> findAllByScenarioId(Long scenarioId);
+	@Query("""
+		SELECT m FROM Mission m
+		LEFT JOIN m.scenario s
+		WHERE s.id = :scenarioId
+		AND s.member.id = :memberId
+		AND (m.useDate IS NULL OR m.useDate = :date)
+		""")
+	List<Mission> findMissionsByScenarioIdWithNullAndDate(Long memberId, Long scenarioId, LocalDate date);
+
+	@Query("""
+		SELECT m FROM Mission m
+		LEFT JOIN m.scenario s
+		WHERE s.id = :scenarioId
+		AND s.member.id = :memberId
+		AND m.useDate = :date
+		""")
+	List<Mission> findMissionsByScenarioIdAndDate(Long memberId, Long scenarioId, LocalDate date);
 
 }
--- src/main/java/com/und/server/scenario/repository/ScenarioRepository.java
+++ src/main/java/com/und/server/scenario/repository/ScenarioRepository.java
@@ -1,5 +1,6 @@
 package com.und.server.scenario.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,15 +16,26 @@ import jakarta.validation.constraints.NotNull;
 public interface ScenarioRepository extends JpaRepository<Scenario, Long> {
 
 	@EntityGraph(attributePaths = {"notification", "missionList"})
-	Optional<Scenario> findById(@NotNull Long id);
+	Optional<Scenario> findByIdAndMemberId(@NotNull Long memberId, @NotNull Long id);
 
 	List<Scenario> findByMemberIdAndNotification_NotifTypeOrderByOrder(Long memberId, NotifType notifType);
 
 	@Query("""
-			SELECT MAX(s.order)
-			FROM Scenario s
-			WHERE s.member.id = :memberId
-				AND s.notification.notifType = :notifType
+		SELECT s FROM Scenario s
+		LEFT JOIN FETCH s.notification
+		LEFT JOIN s.member m
+		LEFT JOIN FETCH s.missionList mission
+		WHERE s.id = :scenarioId
+			AND m.id = :memberId
+			AND (mission.useDate IS NULL OR mission.useDate = :today)
+		""")
+	Optional<Scenario> findByIdWithDefaultMissions(Long memberId, Long scenarioId, LocalDate today);
+
+	@Query("""
+		SELECT MAX(s.order)
+		FROM Scenario s
+		WHERE s.member.id = :memberId
+			AND s.notification.notifType = :notifType
 		""")
 	Optional<Integer> findMaxOrderByMemberIdAndNotifType(Long memberId, NotifType notifType);
 
