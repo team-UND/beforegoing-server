3cde1ba3298e81288174cb5df6a33c54f90b339d
sotogito <sotogitoarchive@gmail.com>
2025-08-11 01:17:27 +0900

    ✨ Add EnableJpaAuditing annotaion to ServerApplication

--- src/main/java/com/und/server/ServerApplication.java
+++ src/main/java/com/und/server/ServerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
 @ConfigurationPropertiesScan
+@EnableJpaAuditing
 public class ServerApplication {
 
 	public static void main(String[] args) {
