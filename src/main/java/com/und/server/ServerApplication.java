469d90da8fb541ee209e6a85ebfa8d5a754f841b
sotogito <sotogitoarchive@gmail.com>
2025-08-11 14:37:09 +0900

    🐛 Replace Spring Data auditing with Hibernate timestamps

--- src/main/java/com/und/server/ServerApplication.java
+++ src/main/java/com/und/server/ServerApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
 @ConfigurationPropertiesScan
-@EnableJpaAuditing
 public class ServerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/und/server/common/entity/BaseTimeEntity.java
+++ src/main/java/com/und/server/common/entity/BaseTimeEntity.java
@@ -2,27 +2,24 @@ package com.und.server.common.entity;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
 @Getter
 
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
-	@CreatedDate
-	@Column(updatable = false, nullable = false)
+	@CreationTimestamp
+	@Column(nullable = false, updatable = false)
 	private LocalDateTime createdAt;
 
-	@LastModifiedDate
-	@Column
+	@UpdateTimestamp
+	@Column(nullable = false)
 	private LocalDateTime updatedAt;
 
 }
