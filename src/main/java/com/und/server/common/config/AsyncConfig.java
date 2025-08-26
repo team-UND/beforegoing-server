04c4114dc10465661e2b89421328ea7240dbfdc4
sotogito <sotogitoarchive@gmail.com>
2025-08-26 13:12:36 +0900

    🔧 Add AsyncConfig

---
+++ src/main/java/com/und/server/common/config/AsyncConfig.java
@@ -0,0 +1,8 @@
+package com.und.server.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig { }
