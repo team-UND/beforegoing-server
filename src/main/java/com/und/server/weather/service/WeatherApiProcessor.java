b64715e61fa8504d7bc675ced9842f7b3f032994
sotogito <sotogitoarchive@gmail.com>
2025-08-29 15:06:06 +0900

    🎨 Refactor Api service

--- src/main/java/com/und/server/weather/service/WeatherApiProcessor.java
+++ src/main/java/com/und/server/weather/service/WeatherApiProcessor.java
@@ -9,19 +9,8 @@ import java.util.concurrent.TimeoutException;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestClientResponseException;
 
-import com.und.server.weather.client.KmaWeatherClient;
-import com.und.server.weather.client.OpenMeteoClient;
-import com.und.server.weather.client.OpenMeteoKmaClient;
-import com.und.server.weather.config.WeatherProperties;
-import com.und.server.weather.constants.FineDustType;
 import com.und.server.weather.constants.TimeSlot;
-import com.und.server.weather.constants.UvType;
-import com.und.server.weather.constants.WeatherType;
 import com.und.server.weather.dto.GridPoint;
 import com.und.server.weather.dto.OpenMeteoWeatherApiResultDto;
 import com.und.server.weather.dto.WeatherApiResultDto;
@@ -41,30 +30,24 @@ import lombok.extern.slf4j.Slf4j;
 public class WeatherApiProcessor {
 
 	private static final long API_TIMEOUT_SEC = 5;
-	private final KmaWeatherClient kmaWeatherClient;
-	private final OpenMeteoClient openMeteoClient;
-	private final OpenMeteoKmaClient openMeteoKmaClient;
-	private final WeatherProperties weatherProperties;
+	private final KmaApiService kmaApiService;
+	private final OpenMeteoApiService openMeteoApiService;
 	private final Executor weatherExecutor;
 
 	public WeatherApiProcessor(
-		KmaWeatherClient kmaWeatherClient,
-		OpenMeteoClient openMeteoClient,
-		OpenMeteoKmaClient openMeteoKmaClient,
-		WeatherProperties weatherProperties,
+		KmaApiService kmaApiService,
+		OpenMeteoApiService openMeteoApiService,
 		@Qualifier("weatherExecutor") Executor weatherExecutor
 	) {
-		this.kmaWeatherClient = kmaWeatherClient;
-		this.openMeteoClient = openMeteoClient;
-		this.openMeteoKmaClient = openMeteoKmaClient;
-		this.weatherProperties = weatherProperties;
+		this.kmaApiService = kmaApiService;
+		this.openMeteoApiService = openMeteoApiService;
 		this.weatherExecutor = weatherExecutor;
 	}
 
 
 	public WeatherApiResultDto callTodayWeather(
 		final WeatherRequest weatherRequest,
-		final TimeSlot currentSlot,
+		final TimeSlot timeSlot,
 		final LocalDate today
 	) {
 		final Double latitude = weatherRequest.latitude();
@@ -72,11 +55,13 @@ public class WeatherApiProcessor {
 		final GridPoint gridPoint = GridConverter.convertToApiGrid(latitude, longitude);
 
 		CompletableFuture<KmaWeatherResponse> weatherFuture =
-			CompletableFuture.supplyAsync(() -> callKmaWeatherApi(gridPoint, currentSlot, today), weatherExecutor)
+			CompletableFuture.supplyAsync(() -> kmaApiService.callWeatherApi(gridPoint, timeSlot, today),
+					weatherExecutor)
 				.orTimeout(API_TIMEOUT_SEC, TimeUnit.SECONDS);
 
 		CompletableFuture<OpenMeteoResponse> openMeteoFuture =
-			CompletableFuture.supplyAsync(() -> callOpenMeteoApi(latitude, longitude, today), weatherExecutor)
+			CompletableFuture.supplyAsync(() -> openMeteoApiService.callDustUvApi(latitude, longitude, today),
+					weatherExecutor)
 				.orTimeout(API_TIMEOUT_SEC, TimeUnit.SECONDS);
 
 		try {
@@ -106,18 +91,21 @@ public class WeatherApiProcessor {
 	public WeatherApiResultDto callFutureWeather(
 		final WeatherRequest weatherRequest,
 		final TimeSlot timeSlot,
-		final LocalDate today, final LocalDate targetDate
+		final LocalDate today,
+		final LocalDate targetDate
 	) {
 		final Double latitude = weatherRequest.latitude();
 		final Double longitude = weatherRequest.longitude();
 		final GridPoint gridPoint = GridConverter.convertToApiGrid(latitude, longitude);
 
 		CompletableFuture<KmaWeatherResponse> weatherFuture =
-			CompletableFuture.supplyAsync(() -> callKmaWeatherApi(gridPoint, timeSlot, today), weatherExecutor)
+			CompletableFuture.supplyAsync(() -> kmaApiService.callWeatherApi(gridPoint, timeSlot, today),
+					weatherExecutor)
 				.orTimeout(API_TIMEOUT_SEC, TimeUnit.SECONDS);
 
 		CompletableFuture<OpenMeteoResponse> openMeteoFuture =
-			CompletableFuture.supplyAsync(() -> callOpenMeteoApi(latitude, longitude, targetDate), weatherExecutor)
+			CompletableFuture.supplyAsync(() -> openMeteoApiService.callDustUvApi(latitude, longitude, targetDate),
+					weatherExecutor)
 				.orTimeout(API_TIMEOUT_SEC, TimeUnit.SECONDS);
 
 		try {
@@ -152,12 +140,13 @@ public class WeatherApiProcessor {
 		final Double longitude = weatherRequest.longitude();
 
 		CompletableFuture<OpenMeteoWeatherResponse> weatherFuture =
-			CompletableFuture.supplyAsync(() -> callOpenMeteoKmaWeatherApi(
-					latitude, longitude, targetDate), weatherExecutor)
+			CompletableFuture.supplyAsync(() -> openMeteoApiService.callWeatherApi(latitude, longitude, targetDate),
+					weatherExecutor)
 				.orTimeout(API_TIMEOUT_SEC, TimeUnit.SECONDS);
 
 		CompletableFuture<OpenMeteoResponse> openMeteoFuture =
-			CompletableFuture.supplyAsync(() -> callOpenMeteoApi(latitude, longitude, targetDate), weatherExecutor)
+			CompletableFuture.supplyAsync(() -> openMeteoApiService.callDustUvApi(latitude, longitude, targetDate),
+					weatherExecutor)
 				.orTimeout(API_TIMEOUT_SEC, TimeUnit.SECONDS);
 
 		try {
@@ -176,128 +165,6 @@ public class WeatherApiProcessor {
 		} catch (Exception e) {
 			log.error("General error during Open-Meteo KMA future day data processing", e);
 			throw new WeatherException(WeatherErrorResult.WEATHER_SERVICE_ERROR, e);
-		}
-	}
-
-
-	private KmaWeatherResponse callKmaWeatherApi(
-		final GridPoint gridPoint,
-		final TimeSlot timeSlot,
-		final LocalDate date
-	) {
-		final String baseDate = WeatherType.getBaseDate(timeSlot, date).format(WeatherType.KMA_DATE_FORMATTER);
-		final String baseTime = WeatherType.getBaseTime(timeSlot);
-
-		try {
-			return kmaWeatherClient.getVilageForecast(
-				weatherProperties.kma().serviceKey(),
-				1,
-				1000,
-				"JSON",
-				baseDate, baseTime,
-				gridPoint.gridX(), gridPoint.gridY()
-			);
-		} catch (ResourceAccessException e) {
-			log.error("KMA timeout/network error baseDate={} baseTime={} grid=({},{}), slot={}",
-				baseDate, baseTime, gridPoint.gridX(), gridPoint.gridY(), timeSlot, e);
-			throw new KmaApiException(WeatherErrorResult.KMA_TIMEOUT, e);
-
-		} catch (HttpClientErrorException e) {
-			log.error("KMA 4xx error baseDate={} baseTime={} grid=({},{}), slot={}, status={}",
-				baseDate, baseTime, gridPoint.gridX(), gridPoint.gridY(), timeSlot, e.getStatusCode().value(), e);
-			throw new KmaApiException(WeatherErrorResult.KMA_BAD_REQUEST, e);
-
-		} catch (HttpServerErrorException e) {
-			log.error("KMA 5xx error baseDate={} baseTime={} grid=({},{}), slot={}, status={}",
-				baseDate, baseTime, gridPoint.gridX(), gridPoint.gridY(), timeSlot, e.getStatusCode().value(), e);
-			throw new KmaApiException(WeatherErrorResult.KMA_SERVER_ERROR, e);
-
-		} catch (RestClientResponseException e) {
-			if (e.getStatusCode().value() == 429) {
-				log.error("KMA 429(rate limit) baseDate={} baseTime={} grid=({},{}), slot={}",
-					baseDate, baseTime, gridPoint.gridX(), gridPoint.gridY(), timeSlot, e);
-				throw new KmaApiException(WeatherErrorResult.KMA_RATE_LIMIT, e);
-			}
-			log.error("KMA response error baseDate={} baseTime={} grid=({},{}), slot={}, status={}",
-				baseDate, baseTime, gridPoint.gridX(), gridPoint.gridY(), timeSlot, e.getStatusCode().value(), e);
-			throw new KmaApiException(WeatherErrorResult.KMA_API_ERROR, e);
-
-		} catch (Exception e) {
-			log.error("KMA call failed(others) baseDate={} baseTime={} grid=({},{}), slot={}",
-				baseDate, baseTime, gridPoint.gridX(), gridPoint.gridY(), timeSlot, e);
-			throw new KmaApiException(WeatherErrorResult.KMA_API_ERROR, e);
-		}
-	}
-
-	private OpenMeteoResponse callOpenMeteoApi(
-		final Double latitude, final Double longitude,
-		final LocalDate date
-	) {
-		final String variables = String.join(
-			",",
-			FineDustType.OPEN_METEO_VARIABLES,
-			UvType.OPEN_METEO_VARIABLES
-		);
-		try {
-			return openMeteoClient.getForecast(
-				latitude,
-				longitude,
-				variables,
-				date.toString(),
-				date.toString(),
-				"Asia/Seoul"
-			);
-		} catch (ResourceAccessException e) {
-			log.error("Open-Meteo timeout/network error lat={}, lon={}, date={}",
-				latitude, longitude, date, e);
-			throw new WeatherException(WeatherErrorResult.OPEN_METEO_TIMEOUT, e);
-
-		} catch (HttpClientErrorException e) {
-			log.error("Open-Meteo 4xx error lat={}, lon={}, date={}, status={}",
-				latitude, longitude, date, e.getStatusCode().value(), e);
-			throw new WeatherException(WeatherErrorResult.OPEN_METEO_BAD_REQUEST, e);
-
-		} catch (HttpServerErrorException e) {
-			log.error("Open-Meteo 5xx error lat={}, lon={}, date={}, status={}",
-				latitude, longitude, date, e.getStatusCode().value(), e);
-			throw new WeatherException(WeatherErrorResult.OPEN_METEO_SERVER_ERROR, e);
-
-		} catch (RestClientResponseException e) {
-			if (e.getStatusCode().value() == 429) {
-				log.error("Open-Meteo 429(rate limit) lat={}, lon={}, date={}",
-					latitude, longitude, date, e);
-				throw new WeatherException(WeatherErrorResult.OPEN_METEO_RATE_LIMIT, e);
-			}
-			log.error("Open-Meteo response error lat={}, lon={}, date={}, status={}",
-				latitude, longitude, date, e.getStatusCode().value(), e);
-			throw new WeatherException(WeatherErrorResult.OPEN_METEO_API_ERROR, e);
-
-		} catch (Exception e) {
-			log.error("Open-Meteo call failed(others) lat={}, lon={}, date={}",
-				latitude, longitude, date, e);
-			throw new WeatherException(WeatherErrorResult.OPEN_METEO_API_ERROR, e);
-		}
-	}
-
-	private OpenMeteoWeatherResponse callOpenMeteoKmaWeatherApi(
-		final Double latitude,
-		final Double longitude,
-		final LocalDate date
-	) {
-		try {
-			return openMeteoKmaClient.getWeatherForecast(
-				latitude,
-				longitude,
-				WeatherType.OPEN_METEO_VARIABLES,
-				date.toString(),
-				date.toString(),
-				"Asia/Seoul"
-			);
-
-		} catch (Exception e) {
-			log.error("Open-Meteo KMA call failed lat={}, lon={}, date={}",
-				latitude, longitude, date, e);
-			throw new WeatherException(WeatherErrorResult.OPEN_METEO_API_ERROR, e);
 		}
 	}
 
