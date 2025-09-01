0de273fc95f10f89bf9833d4633b66379388fae3
sotogito <sotogitoarchive@gmail.com>
2025-09-01 19:49:49 +0900

    🔧 Add Timeconfig

---
+++ src/main/java/com/und/server/common/config/TimeConfig.java
@@ -0,0 +1,17 @@
+package com.und.server.common.config;
+
+import java.time.Clock;
+import java.time.ZoneId;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TimeConfig {
+
+	@Bean
+	public Clock clock() {
+		return Clock.system(ZoneId.of("Asia/Seoul"));
+	}
+
+}
