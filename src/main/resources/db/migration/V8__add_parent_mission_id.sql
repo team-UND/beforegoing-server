c0b23e639ddea1ef83be13a692993ee7be9db6fa
sotogito <sotogitoarchive@gmail.com>
2025-09-02 20:11:05 +0900

    🔧 Modify mission parent mission is alert DDL

--- src/main/resources/db/migration/V8__add_parent_mission_id.sql
+++ src/main/resources/db/migration/V8__add_parent_mission_id.sql
@@ -1,4 +1,6 @@
-ALTER TABLE mission 
-ADD COLUMN parent_mission_id BIGINT NULL,
-ADD CONSTRAINT fk_mission_parent_mission 
-    FOREIGN KEY (parent_mission_id) REFERENCES mission(id) ON DELETE CASCADE;
+ALTER TABLE mission
+    ADD COLUMN parent_mission_id BIGINT NULL;
+
+ALTER TABLE mission
+    ADD CONSTRAINT fk_mission_parent_mission
+        FOREIGN KEY (parent_mission_id) REFERENCES mission(id) ON DELETE CASCADE;
