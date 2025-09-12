8e2c1cceb66a8d39e1ae10312631e41f7fed4a0f
sotogito <sotogitoarchive@gmail.com>
2025-09-12 14:43:46 +0900

    🎨 Refactor ScenarioEventPublisher

--- src/main/java/com/und/server/scenario/event/publisher/ScenarioEventPublisher.java
+++ src/main/java/com/und/server/scenario/event/publisher/ScenarioEventPublisher.java
@@ -1,5 +1,7 @@
 package com.und.server.scenario.event.publisher;
 
+import java.time.LocalDate;
+
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
@@ -15,75 +17,75 @@ import com.und.server.scenario.event.ScenarioOrderUpdateEvent;
 import com.und.server.scenario.event.ScenarioUpdateEvent;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-import java.time.LocalDate;
-
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ScenarioEventPublisher {
 
 	private final ApplicationEventPublisher eventPublisher;
 
-	public void publishCreateEvent(
+	public void publishScenarioCreateEvent(
 		final Long memberId, final Scenario scenario, final NotificationType notificationType
 	) {
-		ScenarioCreateEvent event = new ScenarioCreateEvent(memberId, scenario, notificationType);
+		final ScenarioCreateEvent event = new ScenarioCreateEvent(memberId, scenario, notificationType);
 
 		eventPublisher.publishEvent(event);
 	}
 
-	public void publishUpdateEvent(
+	public void publishMissionCreateEvent(
+		final Long memberId, final Long scenarioId, final LocalDate date
+	) {
+		final MissionCreateEvent event = new MissionCreateEvent(memberId, scenarioId, date);
+
+		eventPublisher.publishEvent(event);
+	}
+
+	public void publishScenarioUpdateEvent(
 		final Long memberId,
 		final Scenario scenario,
 		final Boolean isOldScenarioNotificationActive,
 		final NotificationType newNotificationType
 	) {
-		ScenarioUpdateEvent event = new ScenarioUpdateEvent(
+		final ScenarioUpdateEvent event = new ScenarioUpdateEvent(
 			memberId, scenario, isOldScenarioNotificationActive, newNotificationType);
 
 		eventPublisher.publishEvent(event);
 	}
 
-	public void publishDeleteEvent(
-		final Long memberId,
-		final Long scenarioId,
-		final Boolean isNotificationActive,
-		final NotificationType newNotificationType
-	) {
-		ScenarioDeleteEvent event = new ScenarioDeleteEvent(
-			memberId, scenarioId, isNotificationActive, newNotificationType);
-
-		eventPublisher.publishEvent(event);
-	}
-
-	public void publishActiveUpdateEvent(final Long memberId, final boolean isActive) {
-		ActiveUpdateEvent event = new ActiveUpdateEvent(memberId, isActive);
+	public void publishNotificationActiveUpdateEvent(final Long memberId, final boolean isActive) {
+		final ActiveUpdateEvent event = new ActiveUpdateEvent(memberId, isActive);
 
 		eventPublisher.publishEvent(event);
 	}
 
 	public void publishScenarioOrderUpdateEvent(final Long memberId) {
-		ScenarioOrderUpdateEvent event = new ScenarioOrderUpdateEvent(memberId);
+		final ScenarioOrderUpdateEvent event = new ScenarioOrderUpdateEvent(memberId);
 
 		eventPublisher.publishEvent(event);
 	}
 
-	public void publishMissionCheckUpdateEvent(final Long memberId, final Long scenarioId, final LocalDate date) {
-		MissionCheckUpdateEvent event = new MissionCheckUpdateEvent(memberId, scenarioId, date);
+	public void publishMissionCheckUpdateEvent(
+		final Long memberId, final Long scenarioId, final LocalDate date
+	) {
+		final MissionCheckUpdateEvent event = new MissionCheckUpdateEvent(memberId, scenarioId, date);
+
+		eventPublisher.publishEvent(event);
+	}
+
+	public void publishScenarioDeleteEvent(
+		final Long memberId,
+		final Long scenarioId,
+		final Boolean isNotificationActive,
+		final NotificationType newNotificationType
+	) {
+		final ScenarioDeleteEvent event = new ScenarioDeleteEvent(
+			memberId, scenarioId, isNotificationActive, newNotificationType);
 
 		eventPublisher.publishEvent(event);
 	}
 
 	public void publishTodayMissionDeleteEvent(final Long memberId, final Long scenarioId) {
-		MissionDeleteEvent event = new MissionDeleteEvent(memberId, scenarioId);
-
-		eventPublisher.publishEvent(event);
-	}
-
-	public void publishMissionCreateEvent(final Long memberId, final Long scenarioId, final LocalDate date) {
-		MissionCreateEvent event = new MissionCreateEvent(memberId, scenarioId, date);
+		final MissionDeleteEvent event = new MissionDeleteEvent(memberId, scenarioId);
 
 		eventPublisher.publishEvent(event);
 	}
